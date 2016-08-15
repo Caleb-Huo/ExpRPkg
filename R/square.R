fb863d33c0a214b95596e9a08471da4b6a3e60fc
Caleb <xiaoguang1988@gmail.com>
2016-08-15 10:13:05 -0400

    initial commit

---
+++ R/square.R
@@ -0,0 +1,13 @@
+##' Square Function
+##'
+##' This function is designed to square a function
+##' @title Square a number
+##' @param x number to be squared
+##' @return squared number
+##' @author Caleb
+##' @export
+##' @examples
+##' square(2)
+square <- function(x){
+  x^2
+}
